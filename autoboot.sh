fa6404afd9b496a3f8a01dbbc7bae0824e74a26e
Casper da Costa-Luis <casper.dcl@physics.org>
2019-08-12 15:44:30 +0100

    autoboot tidy logging

--- autoboot.sh
+++ autoboot.sh
@@ -1,19 +1,39 @@
 #!/usr/bin/env bash
 
 # Options
-WORKDIR=${PWD}
+WORKDIR="${PWD}"
+LOG_FILE="$WORKDIR"/conjuring.log
 CUSTOM_DIR='/media/*/*/conjuring/custom'
 CUSTOM_ROOT_FILES="docker-compose.override.yml"
+
+log(){
+  level=$1
+  msg="${@:2}"
+  case $level in
+  I|i|[Ii][Nn][Ff][Oo]*)
+    echo "I:$(date -Is):autoboot:$msg"
+    ;;
+  E|e|[Ee][Rr][Rr]*)
+    echo "E:$(date -Is):autoboot:$msg" 1>&2
+    ;;
+  D|d|[Dd][Ee][Bb]*)
+    ;;
+  *)
+    echo "I:$(date -Is):autoboot:$@"
+    ;;
+  esac
+}
 
 # auto-start WiFi hotspot
 # TODO
 
-# sshserver
+log debug ensuring sshserver &> $LOG_FILE
 sudo service ssh start
 
 # docker container with mounted shared folder(s)
 dcc(){
   pushd $WORKDIR
+  log info docker-compose $@
   docker-compose $@
   popd
 }
@@ -35,26 +55,29 @@ supports_perms(){
   return 2
 }
 
-# monitor for a USB storage device containing additional config
 usb_monitor(){
+  log info monitor for a USB storage device containing additional config
   while [ true ]; do
     if [ -n "$usb_found" ]; then
       ls $CUSTOM_DIR &>/dev/null || usb_found=''
-      [ -z "$usb_found" ] && echo unplugged
+      [ -z "$usb_found" ] && log info unplugged
     else
-      ls $CUSTOM_DIR 2>/dev/null && usb_found="$CUSTOM_DIR" && (
-        echo copying found $CUSTOM_DIR
+      ls $CUSTOM_DIR 1>/dev/null && usb_found="$CUSTOM_DIR" && (
+        log info copying found $CUSTOM_DIR
         cp -Ru $CUSTOM_DIR "$WORKDIR"
-        # pull custom root files from media
+        # rm -rf "$WORKDIR"/custom/home_backup  # TODO: avoid this copy
+        log info pull custom root files from media
         for f in $CUSTOM_ROOT_FILES; do
           [ -f $CUSTOM_DIR/../$f ] && \
          cp -u $CUSTOM_DIR/../$f "$WORKDIR"/../
         done
-        # push homes to media
         if [ -d $CUSTOM_DIR/home_backup ]; then
+         log info push homes to media
          if supports_perms $CUSTOM_DIR/home_backup; then
+           log info inplace
            sudo rsync -au --delete "$WORKDIR"/custom/home/ $CUSTOM_DIR/home_backup
          else
+           log info tar
            pushd "$WORKDIR"/custom/home
            sudo tar -upv --exclude-backups -f $CUSTOM_DIR/home_backup/home.tar *
            popd
@@ -75,8 +98,8 @@ usb_monitor(){
   done
 }
 
-# build and run container
-dccup > $WORKDIR/conjuring.log
+log info build and run container >> $LOG_FILE 2>&1
+dccup >> $LOG_FILE 2>&1
 
-usb_monitor >> $WORKDIR/conjuring.log 2>&1 &
+usb_monitor >> $LOG_FILE 2>&1 &
 wait
