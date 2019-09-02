1b310442692923b27d86a7dd1037c5b227a0b01a
Jon Reades <jon@reades.com>
2019-09-02 11:33:49 +0100

    Added ability to append new user accounts. Think the count my gradually drift off by one on each new run but works well enough.

--- src/create_users.py
+++ src/create_users.py
@@ -1,18 +1,29 @@
 #!/usr/bin/env python
 
+# This script is designed to make it easy (easier) to create the CSV file
+# of usernames and passwords expected by the Conjuring Dockerfile. We need
+# to do this because JupyterHub expects all users to have entries in PAM
+# (or it's equivalent), so as part of the setup we need to create a 'real'
+# user in the container for JupyterHub to use.
+#
+# Please use create_users.py -h to see available options for generating these. 
+# A few parameters are specified within this script since they are unlikely to
+# ever be changed by the user. These can be found just below the import statements.
+
 import os
 import csv 
 import string
 import secrets
-import random 
 import argparse
 
+# Where to find Conjuring's custom directory and, within that, the users file 
+# that will be used to create users in the container.
 custom_dir = 'custom'
 users_file = 'users.csv'
+
+# Some necessary elements of the CSV file and password creation process.
 header = ['Username','Password']
 password_chars = list(string.ascii_letters) + list(string.digits) + ['_',':',';','(',')']
-
-import argparse
 
 parser=argparse.ArgumentParser(
     description='''
@@ -30,26 +41,65 @@ parser.add_argument('--unsafe_pwd', type=str, default=None, help='If you want to
 parser.add_argument('--create_admin', type=bool, default=False, help='Do you want to create an admin user? Defaults to False for basic security reasons.')
 parser.add_argument('--admin_user', type=str, default="admin", help='Name of admin user if not \'admin\' (optional)')
 parser.add_argument('--admin_len', type=int, default=12, help='Length of admin user password (defaults to 12 for security reasons).')
+parser.add_argument('--append', type=bool, default=False, help='If we have already created users but need to add some more then we don\'t want to blow away the existing users.')
 
 args=parser.parse_args()
 
 print("Generating usernames and passwords for use by Conjuring container and JupyterHub...")
 
+# Figure we should reinforce this choice for the user (in case the param name wasn't enough).
 if args.unsafe_pwd is not None:
     print("\tDefaulting to unsafe passwords based on '{0}' + user number".format(args.unsafe_pwd))
 
-with open(os.path.join('.',custom_dir,users_file), 'w', newline='') as csvfile:
+# If we are appending then we need to see where we stopped creating users so that we can 
+# initialise our starting point as the maximum + 1 of the _previous_ list of user numbers.
+start_pt = 1
+write_md = 'w'
+if args.append is True:
+    # Read in the file 
+    with open(os.path.join('.',custom_dir,users_file), 'r') as csvfile:
+        pwd = csv.reader(csvfile, delimiter=',',
+                            quotechar='|')
+        
+        # We're going to try to extract the _last_ username if there's
+        # no admin user, or the _second to last_ username if there is an
+        # admin user. Note that this assumes you're aren't changing the 
+        # settings after running the users script for the first time!
+        try: 
+            last_user = list(pwd)[(-2 if args.create_admin is True else -1)][0] # Ternary operator to switch between -1 and -2 from users list
+            start_pt  = int(last_user.replace(args.usernm,''))+1 # Assume user name template can't change so to just remove the template and take what's left (should be an int)
+            write_md = 'a' # Change the write mode to append
+            args.create_admin = False # Don't re-create the admin user
+        except IndexError:
+            # Suggests that the file exists but is empty/uninitialised, so pass without setting write mode.
+            # You could also get other errors from the above section, but they would suggest that 
+            # user has changed the configuration parameters _after_ initialising the container
+            # which is something we don't want to deal with.
+            pass 
+
+# And write the CSV file
+with open(os.path.join('.',custom_dir,users_file), write_md, newline='') as csvfile:
     pwd = csv.writer(csvfile, delimiter=',',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    pwd.writerow(header)
+    
+    # Only write header row if _not_ appending.
+    if args.append is False: 
+        pwd.writerow(header)
 
-    for i in range(1,args.num+1):
+    # Create new users from starting point (can be 1 for new file or the last
+    # non-admin user created if appending to file)
+    for i in range(start_pt,start_pt+args.num):
+
+        # Generate password according to specified user policy.
         if args.unsafe_pwd is not None:
             pwd.writerow([args.usernm + str(i), args.unsafe_pwd + str(i)])
         else:
             pwd.writerow([args.usernm + str(i), ''.join(secrets.choice(password_chars) for i in range(args.pwdlen))])
-    print("\tCreated username and password for " + str(args.num) + " users.")
+    print("\tCreated username and password for " + ('' if args.append is False else 'another ') + str(args.num) + " users.")
 
+    # Create admin user. This can be set to False above if script detects that 
+    # file already exists so that user doesn't accidentally re-create the
+    # admin user later.
     if args.create_admin is True:
         pwd.writerow([args.admin_user, ''.join(secrets.choice(password_chars) for i in range(args.admin_len))])
         print("\tCreated admin username (" + args.admin_user + ") and password.")
